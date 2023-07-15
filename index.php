93e118f208b2feec1c8b6cb7d44d7953154dfcbb
Jão <joaohenriquerc123@gmail.com>
2023-07-14 22:04:15 -0300

    Planejamento e organização feita

--- index.php
+++ index.php
@@ -3,9 +3,37 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>Torre de Hanói</title>
 </head>
 <body>
-    
+    <main id="viewport">
+        <div class="content">
+            <div class="towers">
+
+            </div>
+
+            <div class="information">
+
+                <div class="rules">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos dolorem sequi ipsum nihil dolor mollitia nam animi asperiores fugit nobis, accusantium debitis eligendi pariatur libero sapiente! Tenetur perferendis similique provident?</p>
+                </div>
+
+                <div class="buttons">
+                    <div class="start">
+                        <button class="btn-start">Começar</button>
+                    </div>
+                    <div class="reset">
+                        <button class="btn-reset">Reiniciar</button>
+                    </div>
+                </div>
+
+                <div class="info">
+                    <p>O famoso jogo da Torre de Hanói é um "quebra-cabeça" que consiste em uma base contendo três pinos, em um dos quais são dispostos alguns discos uns sobre os outros, em ordem crescente de diâmetro, de cima para baixo.</p> 
+                </div>
+
+            </div>
+        </div>
+    </main>
 </body>
 </html>
