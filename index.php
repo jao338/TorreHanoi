c321274a5ae6c8d76c9b6393d759e3167d549a95
Jão <joaohenriquerc123@gmail.com>
2023-07-14 23:03:29 -0300

    Feito, mas ainda pode melhorar. Tem muita div

--- index.php
+++ index.php
@@ -15,21 +15,40 @@
 
             <div class="information">
 
-                <div class="rules">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos dolorem sequi ipsum nihil dolor mollitia nam animi asperiores fugit nobis, accusantium debitis eligendi pariatur libero sapiente! Tenetur perferendis similique provident?</p>
+                <div class="info">
+
+                    <div class="text">
+                        <section>O famoso jogo da Torre de Hanói é um "quebra-cabeça" que consiste em uma base contendo três pinos, em um dos quais são dispostos alguns discos uns sobre os outros, em ordem crescente de diâmetro, de cima para baixo.</section> 
+                    </div>
+
+                    <div class="options">
+                        <input type="date">
+                    </div>
+                
                 </div>
 
                 <div class="buttons">
+                    
                     <div class="start">
                         <button class="btn-start">Começar</button>
                     </div>
+
                     <div class="reset">
                         <button class="btn-reset">Reiniciar</button>
                     </div>
                 </div>
 
-                <div class="info">
-                    <p>O famoso jogo da Torre de Hanói é um "quebra-cabeça" que consiste em uma base contendo três pinos, em um dos quais são dispostos alguns discos uns sobre os outros, em ordem crescente de diâmetro, de cima para baixo.</p> 
+                <div class="rules">
+                    <div class="objective">
+                        <section>Você deve transferir todos os discos da torre A para a torre C. </section>
+                    </div>
+
+                    <div class="list">
+                        <ul>
+                            <li>Você deve mover apenas um disco de cada vez.</li>
+                            <li>Sendo que um disco maior nunca pode ficar em cima de um disco menor.</li>
+                        </ul>
+                    </div>
                 </div>
 
             </div>
